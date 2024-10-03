ff042a831d1ccf457b491b96398630e6f7f352a2
Nicole Volcian <nicoleeevlc@gmail.com>
2024-10-03 18:24:25 -0300

    configuring the part of the total value

--- scripts.js
+++ scripts.js
@@ -6,6 +6,9 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista.
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
+const expensesQuantity = document.querySelector("aside header p span")
+
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -49,6 +52,7 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista.
 function expenseAdd(newExpense) {
     try {
         // Cria o elemento para adicionar o item(li) na lista(ul).
@@ -82,11 +86,20 @@ function expenseAdd(newExpense) {
         .toUpperCase()
         .replace("R$", "")}`
 
+        // Cria o ícone de remover
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
         // Adiciona as informações no item.
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
+
+        //Atualiza os totais
+        updateTotals()
 
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas." )
@@ -94,3 +107,43 @@ function expenseAdd(newExpense) {
     }
 }
 
+// Atualiza os totais
+function updateTotals() {
+    try {
+        // Recupera todos os itens(li) da lista(ul).
+        const items = expenseList.children
+
+        // Atualiza a quantidade de itens da lista.
+        expensesQuantity.textContent = `${items.length} ${
+            items.length > 1 ? "despesas" : "despesa"
+        }`
+
+        // Variável para incrementar o total.
+        let total = 0
+
+        // Percorre cada item(li) da lista(ul).
+        for(let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            // Remover caracteres não numéricos e substitui a vírgula pelo ponto.
+            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace("," , ".")
+
+            // Converte o valor para float.
+            value = parseFloat(value)
+
+            // Verifica se é um número válido.
+            if(isNaN(value)){
+                return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+            }
+
+            // Incrementar o valor total
+            total += Number(value)
+        }
+
+        expensesTotal.textContent = total
+
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais.")
+    }
+}
